c8fb6202967cfbe8df494e6c1b953b9770a403e6
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2023-07-16 12:05:27 -0400

    Add ability to upload image

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Grid } from "@mui/material";
+import { ChangeEvent, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-export default App;
+    const handleUpload = (event: ChangeEvent) => {
+        const file = (event.target as HTMLInputElement).files?.[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImageUrl(reader.result as string);
+        };
+
+        if (file != null) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    return (
+        <Grid container spacing={1} direction="column">
+            <Grid item>
+                {imageUrl && <img src={imageUrl} alt="uploaded" height="300" />}
+            </Grid>
+            <Grid item>
+                <Button variant="contained" component="label">
+                    Upload Image
+                    <input
+                        type="file"
+                        accept="image/*"
+                        hidden
+                        onChange={handleUpload}
+                    />
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
